2d6025486dba145599f6461981c731a878f5cd20
mahammadmomin <mahammadmomin.marolix@gmail.com>
2023-11-21 19:31:14 +0530

    https://github.com/mahammadmomin/mahammad.git

---
+++ 21-11-23.py
@@ -0,0 +1,40 @@
+#Conditionl stetments
+
+age=18
+if age>18:
+    print("you can vote")
+elif age==18:
+    print("you are eligible")
+else:
+    print("you are not eligible")
+
+
+
+
+age=17
+if age>18:
+    print("you can vote")
+elif age==18:
+    print("you are eligible")
+else:
+    print("you are not eligible")
+
+
+age=19
+if age>18:
+    print("you can vote")
+elif age==18:
+    print("you are eligible")
+else:
+    print("you are not eligible")
+
+# nested if
+if True:
+    print("outer if")
+    if True:
+        print("inner if")
+    else:
+        print("inner if")
+else:
+    print("outer else")
+
