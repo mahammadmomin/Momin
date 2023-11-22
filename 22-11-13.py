823337fae2b00c520b866d13d5a7179db421ed16
mahammadmomin <mahammadmomin.marolix@gmail.com>
2023-11-22 15:35:44 +0530

    task

---
+++ 22-11-13.py
@@ -0,0 +1,66 @@
+#Loopin statments
+#for ioop
+for i in range(10):
+    print(i)
+
+#skip
+for i in range(10,20,3):
+    print(i)
+
+#list
+m=[9,5,4,2,7,7,4,6,3,1]
+for i in m:
+    print(i)
+
+#string
+a='Momin'
+for i in a:
+    print(i)
+
+
+#while ioop
+# while True:
+#     print("hi")
+
+Momin=5
+while Momin<10:
+    print("hello",Momin)
+    Momin+=1
+
+#nested loop
+for i in range(1,10):
+    for m in range(1,20):
+        print(i+m)
+
+
+#jumping statment
+#pass
+if True:
+    pass
+
+#break
+for i in "mominmohammad":
+    if i=='h':
+        break
+    print(i)
+
+for i in "JAWAN SHARUK":
+    if i=='R':
+        break
+    print(i)
+
+
+#Continue
+for i in "shiva l vyshu":
+    if i=='l':
+        continue
+    print(i)
+
+
+for m in "mohammad momin":
+    if m==' ':
+        continue
+    print(m)
+
+
+
