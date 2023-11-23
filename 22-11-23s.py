8972e8c482c29c37257588334f076f8255d0a6d1
mahammadmomin <mahammadmomin.marolix@gmail.com>
2023-11-23 10:26:59 +0530

    task

---
+++ 22-11-23s.py
@@ -0,0 +1,46 @@
+#string 
+#upper 
+a="mahammad momin"
+print(a.upper())
+
+#lower
+m="MAROLIX"
+print(m.lower())
+
+#ends with
+python="object orianted porgraming langvage"
+print(python.endswith('e'))
+
+#star with
+color="the color is black"
+print(color.startswith('the'))
+
+
+#relpace
+sv="shiva love vyshu"
+print(sv.replace('love','frnd'))
+
+
+#count
+m="mubashir mohammad"
+print(m.count('m'))
+
+#removeprefix
+h="hi hello"
+print(h.removeprefix('hi '))
+
+#removesuffix
+a="7464 8877"
+print(a.removesuffix("8877"))
+
+#strip,lstrip,rstrip
+king="   king qween   "
+print(king)
+print(len(king))
+v=king.strip()
+print(len(v))
+v=king.lstrip()
+print(len(v))
+v=king.rstrip()
+print(v)
+
